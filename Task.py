c47f51a6146dab00730bb8f5dabb8e0910f55a79
Devanshpratapsingh28 <devanshprataps6@gmail.com>
2024-06-15 19:39:22 +0530

    Task Class Created

--- Task.py
+++ Task.py
@@ -1,1 +1,25 @@
+class Task:
+    def __init__(self,title,description,priority,deadline):#Priority=Urgent>High>Low
+        self.title=title
+        self.description=description
+        self.priority=priority
+        self.deadline=deadline
+        self.status="Pending" #Pending/In-Progress/Completed
+
+    def update_task(self, title=None, description=None, priority=None, deadline=None):
+        if title:
+            self.title = title
+        if description:
+            self.description = description
+        if priority:
+            self.priority = priority
+        if deadline:
+            self.deadline = deadline
+
+    def taskCompleted(self,completionDate):
+        self.status="Completed" 
+        self.completionDate= completionDate 
+
+    def task_In_progress(self):
+        self.status="In-Progress"        
 
