3d2c86707995677814c76f33b2679341d2cb7b0c
Devanshpratapsingh28 <devanshprataps6@gmail.com>
2024-06-15 22:00:59 +0530

    ""

--- Task.py
+++ Task.py
@@ -23,23 +23,7 @@ class Task:
     def task_Incomplete(self):
         self.status="Incomplete"   
 
-class WorkTask(Task):
-    def __init__(self,title,description,priority,deadline,project):
-        super().__init__(self,title,description,priority,deadline)
-        self.project=project
 
-    def update_task(self, title=None, description=None, priority=None, deadline=None, project=None):
-        super().update_task(self, title=None, description=None, priority=None, deadline=None)
-        self.project=project
-
-class PersonalTask(Task):
-    def __init__(self,title,description,priority,deadline,category):
-        super().__init__(self,title,description,priority,deadline)
-        self.category=category
-
-    def update_task(self, title=None, description=None, priority=None, deadline=None, category=None):
-        super().update_task(self, title=None, description=None, priority=None, deadline=None)
-        self.category=category
 
 class User:
     def __init__(self,name):
@@ -65,6 +49,9 @@ class User:
 
 #Task Management Functions
 
-
+class TaskManager(Task,User):
+    def __init__(self,name):
+        super(User).__init__(name)
+        
 
 
