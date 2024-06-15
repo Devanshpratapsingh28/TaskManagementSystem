4d77d17d2f3b818b21fa661591da3d1fde0071a0
Devanshpratapsingh28 <devanshprataps6@gmail.com>
2024-06-15 22:52:01 +0530

    c

--- Task.py
+++ Task.py
@@ -32,19 +32,30 @@ class User:
 
     def addTask(self,task):
         self.taskList.append(task) 
-        print("Task added successfully") 
+        # print("Task added successfully") 
 
     def removeTask(self,task):
         self.taskList.remove(task)
         print("Task removed successfully") 
-
-    def getTasks(self, status=None):
+    
+    #Track based on status
+    def getTasksStatus(self, status=None):
         if status:
             tasks_with_status = []
             for task in self.taskList:
                 if task.status == status:
                     tasks_with_status.append(task)
             return tasks_with_status
+        return self.taskList
+    
+    #Track based on priority
+    def getTasksPriority(self, priority=None):
+        if priority:
+            tasks_with_priority = []
+            for task in self.taskList:
+                if task.priority == priority:
+                    tasks_with_priority.append(task)
+            return tasks_with_priority
         return self.taskList
 
 
@@ -60,22 +71,22 @@ class TaskManager(User):
         super().removeTask(task)
 
 #Examples for testing
-task1 = Task("Task 1", "Description 1", "High", "2024-06-30")
-task2 = Task("Task 2", "Description 2", "Low", "2024-07-15")
+
+taskList=(("Task 1", "Go to market", "Low", "2024-06-30"),("Task 2", "Meeting on Friday", "High", "2024-06-21"),("Task 3", "Call Milkman", "Low", "2024-06-16"),Task("Task 4", "Book Ticket", "High", "2024-06-30"),("Task 5", "Schedule meeting with Virat", "High", "2024-06-18"))
 
 user = TaskManager("Alice")
-user.addTask(task1)
-user.addTask(task2)
+for i in taskList:
+    user.addTask(i)
 
-# List all tasks
-for task in user.getTasks():
-    print(task.title, task.status)
+# Give List all tasks Priority wise
+for task in user.getTasksPriority("High"):
+    print(task.title, task.priority)
 
 # Mark task1 as completed
-task1.taskCompleted("2024-06-20")
+taskList[0].taskCompleted("2024-06-20")
 
 # List completed tasks
-completed_tasks = user.getTasks(status="Completed")
+completed_tasks = user.getTasksStatus(status="Completed")
 for task in completed_tasks:
     print(task.title, task.status)        
         
