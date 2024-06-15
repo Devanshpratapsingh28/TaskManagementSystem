36e9381b6d3a0a92477a6ef1aa32bd35036a8d6f
Devanshpratapsingh28 <devanshprataps6@gmail.com>
2024-06-15 19:57:54 +0530

    All Class Created

--- Task.py
+++ Task.py
@@ -23,6 +23,24 @@ class Task:
     def task_In_progress(self):
         self.status="In-Progress"   
 
+class WorkTask(Task):
+    def __init__(self,title,description,priority,deadline,project):
+        super().__init__(self,title,description,priority,deadline)
+        self.project=project
+
+    def update_task(self, title=None, description=None, priority=None, deadline=None, project=None):
+        super().update_task(self, title=None, description=None, priority=None, deadline=None)
+        self.project=project
+
+class PersonalTask(Task):
+    def __init__(self,title,description,priority,deadline,category):
+        super().__init__(self,title,description,priority,deadline)
+        self.category=category
+
+    def update_task(self, title=None, description=None, priority=None, deadline=None, category=None):
+        super().update_task(self, title=None, description=None, priority=None, deadline=None)
+        self.category=category
+
 class User:
     def __init__(self,name):
         self.name=name
@@ -38,7 +56,7 @@ class User:
 
     def get_Tasks(self):
         return self.taskList  
-    
-      
+
+
 
 
